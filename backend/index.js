166f9805dd4ab1f67a5e84ecd2675048498ace1b
ken-rolex <rajan.jha114430@gmail.com>
2026-03-31 17:21:51 +0530

    add vercel config

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,11 @@ app.use('/student',studentroute);
 app.use('/company',companyroute);
 app.use('/query',queryroute)
 Connectdb();
-app.listen(3000,()=>{
-    console.log('server stared on port 3000');
-})
+
+if (process.env.NODE_ENV !== 'production') {
+    app.listen(3000,()=>{
+        console.log('server stared on port 3000');
+    });
+}
+
+export default app;
